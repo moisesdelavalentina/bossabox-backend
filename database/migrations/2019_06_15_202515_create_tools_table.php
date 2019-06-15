d561792b0892430d6ee3cfbc4096430aa2b2d2d7
Moisés Delavalentina <moises_delavalentina@yahoo.com.br>
2019-06-15 19:53:27 -0400

    Inicial Commit

---
+++ database/migrations/2019_06_15_202515_create_tools_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tools', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('title', 45);
+            $table->char('link', 200);
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tools');
+    }
+}
---
+++ database/migrations/2019_06_15_205007_alter_tags_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tags', function (Blueprint $table) {          
+            $table->unsignedInteger('tool_id');
+            $table->foreign('tool_id')->references('id')->on('tools');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
